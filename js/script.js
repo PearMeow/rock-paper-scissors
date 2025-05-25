3d3566f5244a5ed9db66b6ef7d8e0706405334e0
pearmeow <perryhuang2015@gmail.com>
2025-05-25 16:26:27 -0400

    Complete rest of functions surrounding game

--- js/script.js
+++ js/script.js
@@ -20,7 +20,50 @@ function getHumanChoice() {
     return choice;
 }
 
-console.log(getHumanChoice());
+function humanWins(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        return true;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        return true;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        return true;
+    }
+    return false;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie: ${humanChoice} is the same as ${computerChoice}.`);
+    }
+    if (humanWins(humanChoice, computerChoice)) {
+        console.log(`You win: ${humanChoice} beats ${computerChoice}.`);
+        ++humanScore;
+    } else {
+        console.log(`You lose: ${computerChoice} beats ${humanChoice}.`);
+        ++computerScore;
+    }
+}
+
+function playGame(rounds = 5) {
+    for (let i = 0; i < rounds; ++i) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    console.log(`Final score: ${humanScore}(You) to ${computerScore}(CPU)`);
+    if (humanScore > computerScore) {
+        console.log("You win!");
+    } else if (humanScore < computerScore) {
+        console.log("You lose!");
+    } else {
+        console.log("It's a tie...");
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playGame();
+
+// console.log(getHumanChoice());
 
 // for (let i = 0; i < 10; ++i) {
 //     console.log(getComputerChoice());
